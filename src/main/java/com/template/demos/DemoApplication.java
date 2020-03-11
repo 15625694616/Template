9637fa3d9f48ab4ee95b8f245a6a22aa2a164b05
Administrator <616058887@qq.com>
2020-03-11 10:05:32 +0800

    新增防御XSS攻击功能

--- src/main/java/com/template/demos/DemoApplication.java
+++ src/main/java/com/template/demos/DemoApplication.java
@@ -2,12 +2,17 @@ package com.template.demos;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.template.demos.config.AuthorizationInterceptor;
+import com.template.demos.utils.xss.XssFilter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Administrator
@@ -40,5 +45,19 @@ public class DemoApplication implements WebMvcConfigurer {
 	public PaginationInterceptor paginationInterceptor() {
 		PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
 		return paginationInterceptor;
+	}
+
+	@Bean
+	public FilterRegistrationBean xssFilterRegistrationBean() {
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(new XssFilter());
+		filterRegistrationBean.setOrder(1);
+		filterRegistrationBean.setEnabled(true);
+		filterRegistrationBean.addUrlPatterns("/*");
+		Map<String, String> initParameters = new HashMap<String, String>();
+		initParameters.put("excludes", "/favicon.ico,/img/*,/js/*,/css/*");
+		initParameters.put("isIncludeRichText", "true");
+		filterRegistrationBean.setInitParameters(initParameters);
+		return filterRegistrationBean;
 	}
 }
